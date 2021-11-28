64338fd2497e49d358ea064dca5889d52c4a6001
Jason Cory Alvernaz <jason@twentytwolabs.com>
2021-11-28 12:37:04 -0800

    Add SEO plugins to Gatsby

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -48,6 +48,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

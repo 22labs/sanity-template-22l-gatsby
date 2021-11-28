95efddda094a2b922d5c34308084637e92d6242d
Jason Cory Alvernaz <jason@twentytwolabs.com>
2021-11-28 14:28:08 -0800

    Update Prettier config

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  parser: 'typescript',
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}

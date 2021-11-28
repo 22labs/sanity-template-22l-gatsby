71f946fd19c3ec106eff186029709328cef5e0e7
Jason Cory Alvernaz <jason@twentytwolabs.com>
2021-11-28 13:53:55 -0800

    Update frontend to use `projectTitle`

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -2,7 +2,7 @@ const sanityClientConfig = require('./src/sanity/client-config')
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `<#< sanity.projectTitle >#>`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
